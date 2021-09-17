b5b319d9eab9d338d1ed6f0e587a503d34d625f5
howardtrim <howardtrim@gmail.com>
2021-09-17 16:29:41 +0200

    Update streamlit_setup.sh

--- streamlit_setup.sh
+++ streamlit_setup.sh
@@ -1,6 +1,2 @@
 mkdir -p ~/.streamlit
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
+echo "[server] headless = true port = $PORT enableCORS = false" > ~/.streamlit/config.toml
